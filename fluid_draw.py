8bc91cd9828a6e808a8fd8880ef2c507e8a0e41b
Timdnb <timdnblanken@gmail.com>
2021-09-14 19:07:26 +0200

    Update velocity field

--- fluid_draw.py
+++ fluid_draw.py
@@ -46,15 +46,13 @@ def draw(dens, x_vel, y_vel):
         ele = floor(mouse_pos[0] / (w_scr/grid_size))
         row = floor(mouse_pos[1] / (h_scr/grid_size))
 
-        dens[row,ele] += 100
-        x_vel[row,ele] += 0.8
-        y_vel[row,ele] += 0.8
+        dens[row,ele] += 20
 
-        # Prevent velocities from getting bigger than one
-        if abs(x_vel[row,ele]) > 1:
-            x_vel[row,ele] = 1
-        if abs(y_vel[row,ele]) > 1:
-            y_vel[row,ele] = 1
+        # Give the 4 surrounding squares velocity to spread
+        # y_vel[row-1,ele] += 0.8
+        # y_vel[row+1,ele] -= 0.8
+        # x_vel[row,ele-1] -= 0.8
+        # x_vel[row,ele+1] += 0.8
 
     # Find max density, black will be assigned to this density
     max_dens = 50
@@ -65,61 +63,49 @@ def draw(dens, x_vel, y_vel):
             x = ele*(w_scr/grid_size)
             y = row*(h_scr/grid_size)
 
+            cell_width = w_scr/grid_size
+
             # Make color based on density (lower density = lighter color)
-            c_dens = 255 - int((dens[row,ele]/max_dens)*255) 
+            c_dens = int((dens[row,ele]/max_dens)*255) 
             if c_dens < 0:
                 c_dens = 0  
             if c_dens > 255:
                 c_dens = 255      
-            color = (c_dens,c_dens,c_dens)
+            color = pg.Color(c_dens,c_dens,c_dens)
 
             # Position arrow
             arrow_rect.x = x
             arrow_rect.y = y
 
             # Calculate angle for velocity vector
-            angle = atan2(y_vel[row,ele],x_vel[row,ele]) * (180/np.pi)    
+            angle = atan2(y_vel[row,ele],x_vel[row,ele]) #* (180/np.pi)   
+            speed = (y_vel[row,ele]**2 + x_vel[row,ele]**2) ** (1/2)
 
-            # Resize and rotate
-            arrow_siz = pg.transform.smoothscale(arrow, (int(0.7*(w_scr/grid_size)),int(0.7*(h_scr/grid_size))))
-            arrow_ang = pg.transform.rotate(arrow_siz, angle)
+            if speed >= 0:
+                color_s = color1
+            if speed > 0.16:
+                color_s = color2
+            if speed > 0.32:
+                color_s = color3
+            if speed > 0.49:
+                color_s = color4
+            if speed > 0.65:
+                color_s = color4
+            if speed > 0.91:
+                color_s = color5
+            if speed > 0.99:
+                color_s = color6 
+
+            # # Resize and rotate
+            # arrow_siz = pg.transform.smoothscale(arrow, (int(0.7*(w_scr/grid_size)),int(0.7*(h_scr/grid_size))))
+            # arrow_ang = pg.transform.rotate(arrow_siz, angle)
 
             # Draw rectangles and blit arrow on the screen
             pg.draw.rect(screen, color, (x, y, w_scr/grid_size, h_scr/grid_size))
+            pg.draw.aaline(screen, color_s, (x+cell_width/2,y+cell_width/2), (x+cell_width/2*(1+np.cos(angle)), y-cell_width/2*(-1+np.sin(angle))))
+            # pg.draw.circle(screen, color_s, (x+cell_width/2*(1.2+np.cos(angle)), y-cell_width/2*(-1.2+np.sin(angle))), 2)
+            # pg.draw.circle(screen, color_s, (x+cell_width/2,y+cell_width/2), 1)
 
-            screen.blit(arrow_ang, arrow_rect)
-
-# Draw arrows using lines --------------------------------------------------------------------------------
-    # for row in range(grid_size):
-    #     for ele in range(grid_size):
-    #         x = ele*(w_scr/grid_size)
-    #         y = row*(h_scr/grid_size)
-
-    #         cell_width = w_scr/grid_size
-
-    #         # Position arrow
-    #         arrow_rect.x = x
-    #         arrow_rect.y = y
-
-    #         # Calculate length and angle for velocity vector
-    #         angle = atan2(y_vel[row,ele],x_vel[row,ele]) #* (180/np.pi)
-    #         speed = (y_vel[row,ele]**2 + x_vel[row,ele]**2) ** (1/2)
-
-    #         if speed >= 0:
-    #             color_s = color1
-    #         if speed > 0.16:
-    #             color_s = color2
-    #         if speed > 0.32:
-    #             color_s = color3
-    #         if speed > 0.49:
-    #             color_s = color4
-    #         if speed > 0.65:
-    #             color_s = color4
-    #         if speed > 0.91:
-    #             color_s = color5
-    #         if speed > 0.99:
-    #             color_s = color6
-
-    #         pg.draw.aaline(screen, color_s, (x+cell_width/2,y+cell_width/2), (x+cell_width/2*np.cos(angle), y+cell_width/2*np.sin(angle)))
+            # screen.blit(arrow_ang, arrow_rect)
 
     pg.display.flip()
